0182359a45fa806b6a07fe5e53f5cb6c57105ad0
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-28 19:44:44 -0300

    Resolvido

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+
+Console.BackgroundColor = ConsoleColor.Red;
+Console.WriteLine(@"
+Contra todo ditador que no Timão quiser mandar
+Os Gaviões nasceram pra poder reivindicar
+O direito da Fiel que paga ingresso sem parar
+Não temos medo de acabar
+Corinthians joga eu vou tá lá
+Nossa corrente é forte e jamais se quebrará");
+Console.ResetColor();
+Thread.Sleep(3000);
+
+Console.ForegroundColor = ConsoleColor.Black;
+Console.WriteLine(@"
+Pelo Corinthians
+Com muito amor
+Até o fim
+GA-VI-ÕES
+FIEL!");
+Thread.Sleep(3000);
+
+Console.ResetColor();
+Console.BackgroundColor = ConsoleColor.White;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.WriteLine(@"
+Eu sou
+da Gaviões eu sou
+Corinthians joga eu vou
+e ninguém vai me segurar");
+Console.ResetColor();
